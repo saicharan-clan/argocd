7a86a19cd362958f2dbfb780bf15f7f57aa66f47
saicharan-clan <ssaicharan0990@gmail.com>
2025-03-27 18:34:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+@app.route('/')
+def hello_world():
+    return 'Welcome to Sai Charan Clan !!!'
